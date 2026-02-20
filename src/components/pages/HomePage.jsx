498bdf4faa295cbadbfc09ad96f938dba0c8c462
gaikiir <gaichris380@gmail.com>
2026-02-20 22:38:28 +0300

    feat: add HomePage page component

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,57 +1,18 @@
-import UserCarouselData from "../hooks/UserDataCarousel";
-import MainCarousel from "../layouts/common/Carousel";
-import GridItem from "../layouts/common/GridItems";
-import EmptyState from "../layouts/utilities/EmptyState";
-import ErrorMessage from "../layouts/utilities/Error";
-import IsLoading from "../layouts/utilities/Loading";
+import FeaturesIcons from "../common/FeatureIcons";
+import HeroCarousel from "../layouts/Carousel";
+import ChiefsSection from "../layouts/Chefs";
+import { Container } from "../layouts/Container";
+import Products from "../layouts/Products";
+import Testimonial from "../layouts/Testimonial";
 
-export default function HomeComponent() {
-  const { data, loading, error, refetch } = UserCarouselData();
-
-  if (loading) {
-    return (
-      <section className="bg-gray-100 w-full max-w-[90rem] mx-auto">
-        <header className="mt-20">
-          <IsLoading message="Loading carousel..." height="400px" />
-        </header>
-      </section>
-    );
-  }
-
-  if (error) {
-    return (
-      <section className="bg-gray-100 w-full max-w-[90rem] mx-auto">
-        <header className="mt-20">
-          <ErrorMessage
-            title="Failed to load carousel"
-            message={error}
-            height="400px"
-            onRetry={refetch}
-          />
-        </header>
-      </section>
-    );
-  }
-
-  if (!data || data.length === 0) {
-    return (
-      <section className="bg-gray-100 w-full max-w-[90rem] mx-auto">
-        <header className="mt-20">
-          <EmptyState
-            title="No carousel items"
-            message="There are no items to display at the moment."
-            height="400px"
-          />
-        </header>
-      </section>
-    );
-  }
+export default function HomePageComponent() {
   return (
-    <>
-      <section className="w-full h-full max-w-[90rem] mx-auto">
-        <MainCarousel data={data} />
-        <GridItem />
-      </section>
-    </>
+    <Container>
+      <HeroCarousel />
+      <Products />
+      <ChiefsSection/>
+      <FeaturesIcons />
+      <Testimonial/>
+    </Container>
   );
 }
