66704c6e7beceb1b27496bd6173e90cd99d3c31b
gaikiir <gaichris380@gmail.com>
2026-02-27 19:53:20 +0300

    fata:implement menu component

---
+++ src/components/pages/MenuPage.jsx
@@ -0,0 +1,12 @@
+import { Container } from "../layouts/Container";
+import Products from "../layouts/Products";
+
+export default function MenuPage() {
+  return (
+    <Container>
+      <div className="mt-28">
+        <Products />
+      </div>
+    </Container>
+  );
+}
